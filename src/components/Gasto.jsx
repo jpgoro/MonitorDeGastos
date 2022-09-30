057a3e94c543af3858f769b27263f25e2dd80f02
Juan Gorosito <jpablo.gorosito87@gmail.com>
2022-09-30 14:21:21 -0300

    Formateo de fecha

--- src/components/Gasto.jsx
+++ src/components/Gasto.jsx
@@ -1,13 +1,20 @@
+import { formatearFecha } from "../helpers";
+
 const Gasto = ({ gasto }) => {
-  const { categoria, nombre, cantidad, id } = gasto;
+  const { categoria, nombre, cantidad, id, fecha } = gasto;
   return (
     <div className="gasto sombra">
       <div className="contenido-gasto">
+        <img src="" alt="" />
         <div className="descripcion-gasto">
           <p className="categoria">{categoria}</p>
           <p className="nombre-gasto">{nombre}</p>
+          <p className="fecha-gasto">
+            Agregado el: <span>{formatearFecha(fecha)}</span>
+          </p>
         </div>
       </div>
+        <p className="cantidad-gasto">${cantidad}</p>
     </div>
   );
 };
